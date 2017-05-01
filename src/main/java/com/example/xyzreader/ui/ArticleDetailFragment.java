24e7545200ee01b6dd1dd7512e8d1180ca8889f0
Ben Medcalf <ben.medcalf@gmail.com>
2017-05-01 13:15:27 -0400

    Lots of code cleanup per my review.
    - Deleted unused classes (DividerItemDecoration, ObservableScrollView, MaxWidthLinearLayout) that were initially provided with the project, but no longer serve a purpose since my material refactor.
    - Removed unused imports and member variables throughout the codebase.

--- src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
+++ src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
@@ -6,13 +6,11 @@ import android.content.Loader;
 import android.database.Cursor;
 import android.graphics.Rect;
 import android.graphics.Typeface;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
-import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.widget.Toolbar;
 import android.text.Html;
 import android.text.format.DateUtils;
@@ -43,23 +41,10 @@ public class ArticleDetailFragment extends Fragment implements
     private static final String TAG = "ArticleDetailFragment";
 
     public static final String ARG_ITEM_ID = "item_id";
-    private static final float PARALLAX_FACTOR = 1.25f;
-
     private Cursor mCursor;
     private long mItemId;
     private View mRootView;
-    private int mMutedColor = 0xFF333333;
-    private ObservableScrollView mScrollView;
-    private CoordinatorLayout mCoordinatorLayout;
-    private ColorDrawable mStatusBarColorDrawable;
-
-    private int mTopInset;
-    private View mPhotoContainerView;
     private ImageView mPhotoView;
-    private int mScrollY;
-    private boolean mIsCard = false;
-    private int mStatusBarFullOpacityBottom;
-    private String mTransitionName;
     private static final String ARG_ARTICLE_POSITION = "arg_article_position";
     private static final String ARG_STARTING_ARTICLE_IMAGE_POSITION = "arg_starting_article_image_position";
     private int mCurrentPosition;
@@ -109,18 +94,10 @@ public class ArticleDetailFragment extends Fragment implements
             mItemId = getArguments().getLong(ARG_ITEM_ID);
         }
 
-        mIsCard = getResources().getBoolean(R.bool.detail_is_card);
-        mStatusBarFullOpacityBottom = getResources().getDimensionPixelSize(
-                R.dimen.detail_card_top_margin);
         //TODO: change ARG_STAR... to be static member in ArticleListActivity
         mStartingPosition = getArguments().getInt(ARG_STARTING_ARTICLE_IMAGE_POSITION);
         mCurrentPosition = getArguments().getInt(ARG_ARTICLE_POSITION);
         setHasOptionsMenu(true);
-    }
-
-    public ArticleDetailActivity getActivityCast()
-    {
-        return (ArticleDetailActivity) getActivity();
     }
 
     @Override
@@ -140,8 +117,6 @@ public class ArticleDetailFragment extends Fragment implements
                              Bundle savedInstanceState)
     {
         mRootView = inflater.inflate(R.layout.fragment_article_detail_redo, container, false);
-        mCoordinatorLayout = (CoordinatorLayout)
-                mRootView.findViewById(R.id.coordinator_layout_detail_rootview);
         mToolbar = (Toolbar) mRootView.findViewById(R.id.detail_toolbar);
         mPhotoView = (ImageView) mRootView.findViewById(R.id.toolbar_article_image);
         mPhotoView.setTransitionName(Constants.ARTICLES.get(mCurrentPosition));
@@ -157,7 +132,6 @@ public class ArticleDetailFragment extends Fragment implements
         {
             return;
         }
-//        TextView titleView = (TextView) mRootView.findViewById(R.id.article_title);
         TextView bylineView = (TextView) mRootView.findViewById(R.id.article_byline);
         bylineView.setMovementMethod(new LinkMovementMethod());
         TextView bodyView = (TextView) mRootView.findViewById(R.id.article_body);
@@ -173,15 +147,28 @@ public class ArticleDetailFragment extends Fragment implements
             mRootView.setVisibility(View.VISIBLE);
             mRootView.animate().alpha(1);
             mToolbar.setTitle(mCursor.getString(ArticleLoader.Query.TITLE));
-//            titleView.setText(mCursor.getString(ArticleLoader.Query.TITLE));
-            bylineView.setText(Html.fromHtml(
-                    DateUtils.getRelativeTimeSpanString(
-                            mCursor.getLong(ArticleLoader.Query.PUBLISHED_DATE),
-                            System.currentTimeMillis(), DateUtils.HOUR_IN_MILLIS,
-                            DateUtils.FORMAT_ABBREV_ALL).toString()
-                            + " by <font color='#ffffff'>"
-                            + mCursor.getString(ArticleLoader.Query.AUTHOR)
-                            + "</font>"));
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
+            {
+                bylineView.setText(Html.fromHtml(DateUtils.getRelativeTimeSpanString(
+                        mCursor.getLong(ArticleLoader.Query.PUBLISHED_DATE),
+                        System.currentTimeMillis(), DateUtils.HOUR_IN_MILLIS,
+                        DateUtils.FORMAT_ABBREV_ALL).toString()
+                        + " by <font color='#ffffff'>"
+                        + mCursor.getString(ArticleLoader.Query.AUTHOR)
+                        + "</font>", Html.FROM_HTML_MODE_LEGACY));
+            }
+            else
+            {
+                bylineView.setText(Html.fromHtml(
+                        DateUtils.getRelativeTimeSpanString(
+                                mCursor.getLong(ArticleLoader.Query.PUBLISHED_DATE),
+                                System.currentTimeMillis(), DateUtils.HOUR_IN_MILLIS,
+                                DateUtils.FORMAT_ABBREV_ALL).toString()
+                                + " by <font color='#ffffff'>"
+                                + mCursor.getString(ArticleLoader.Query.AUTHOR)
+                                + "</font>"));
+            }
             bodyView.setText(Html.fromHtml(mCursor.getString(ArticleLoader.Query.BODY)));
             RequestCreator articleImageRequest = Picasso.with(getActivity())
                     .load(mCursor.getString(ArticleLoader.Query.PHOTO_URL));
@@ -230,19 +217,6 @@ public class ArticleDetailFragment extends Fragment implements
     {
         mCursor = null;
         bindViews();
-    }
-
-    public int getUpButtonFloor()
-    {
-        if (mPhotoContainerView == null || mPhotoView.getHeight() == 0)
-        {
-            return Integer.MAX_VALUE;
-        }
-
-        // account for parallax
-        return mIsCard
-                ? (int) mPhotoContainerView.getTranslationY() + mPhotoView.getHeight() - mScrollY
-                : mPhotoView.getHeight() - mScrollY;
     }
 
     public void startPostponedTransition()
